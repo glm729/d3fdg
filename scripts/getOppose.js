321e446b3946bd687e3a6085f75383ff08fc0d70
glm729 <georgemalone@outlook.com.au>
2021-02-02 16:37:40 +0800

    Rewrote getOppose.js
    - Used filter as opposed to .map( ... ).filter(x => x !== undefined)
    - Not yet performance tested, but doesn't appear to damage data
      processing time from a quick check
    - ChemInfo script not updated here yet

--- scripts/getOppose.js
+++ scripts/getOppose.js
@@ -10,37 +10,33 @@
  * compound pair in the literature data.
  */
 function getOppose(withId, kre) {
-  // Initialise the results array
-  let result = [];
-  // Get all compound IDs in the input data
-  let idAll = withId
-    .map(w => {  // All IDs per entry
-      return [w.idAnchor, w.idOther].flat();
-    })
-    .flat()  // Flatten the array of arrays
-    .filter(x => x !== null)  // Filter out the nulls (from idOther)
-  // Get the unique IDs present
-  let idUniq = [...new Set(idAll)];
-  // Map over the KEGG reaction equations data
-  kre.map(k => {
-    // Get the literature compound IDs, LHS
-    let lhsIn = k.lhs.map(l => {
-      if (idUniq.indexOf(l) !== -1) return l;
-    }).filter(l => l !== undefined);
-    // If none to link, next
-    if (lhsIn.length === 0) return;
-    // Get the literature compound IDs, RHS
-    let rhsIn = k.rhs.map(r => {
-      if (idUniq.indexOf(r) !== -1) return r;
-    }).filter(r => r !== undefined);
-    // If none to link, next
-    if (rhsIn.length === 0) return;
-    // Push every opposing pair  -- loop over the LHS and RHS
-    lhsIn.map(l => {
-      rhsIn.map(r => {
-        result.push({"lhs": l, "rhs": r});
-      });
-    });
+  // Initialise data out and array of all IDs
+  let out = [];
+  let idAll = [];
+  // Map over the data with IDs
+  withId.map(w => {
+    // If the anchor ID isn't already present, push to idAll
+    if (idAll.indexOf(w.idAnchor) === -1) idAll.push(w.idAnchor);
+    // If any other IDs
+    if (w.idOther !== null) {
+      // For each other ID, if not present, push to idAll
+      w.idOther.map(o => {if (idAll.indexOf(o) === -1) {idAll.push(o)}});
+    };
   });
-  return result;
+  // Get unique IDs
+  let idUniq = [...new Set(idAll)];
+  // Map over the KEGG Reaction data
+  kre.map(k => {
+    // Get literature compound IDs on the LHS
+    let lhsIn = k.lhs.filter(l => idUniq.indexOf(l) !== -1);
+    // If none, quit
+    if (lhsIn.length === 0) return;
+    // Get literature compound IDs on the RHS
+    let rhsIn = k.rhs.filter(r => idUniq.indexOf(r) !== -1);
+    // If none, quit
+    if (rhsIn.length === 0) return;
+    // Map over the LHS and RHS, and push the opposing compound IDs
+    lhsIn.map(l => {rhsIn.map(r => {out.push({"lhs": l, "rhs": r})})});
+  });
+  return out;
 }
