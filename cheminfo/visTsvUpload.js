83464e6d7b1f61be92193aff14d4d39228c3e092
glm729 <georgemalone@outlook.com.au>
2021-02-17 16:13:26 +0800

    Added cell to handle vis parameters TSV upload
    - Overwrites node selection
      - Should be improved / modified in future to avoid this
    - Closes #11, closes #12

---
+++ cheminfo/visTsvUpload.js
@@ -0,0 +1,32 @@
+// Assign an empty header to a JSON if no header is available
+function assignEmptyHeader(ofLength) {
+  let result = [];
+  for (let i = 0; i < ofLength; ++i) {
+    result.push(`v${String(i).padStart(String(ofLength).length, '0')}`);
+  };
+  return result;
+};
+
+// Convert a raw TSV into a JSON
+function convRawTsvJson(raw, header = true) {
+  let result = [];
+  let head;
+  let split = raw
+    .replace(/\n$/, '')
+    .split(/\n/)
+    .map(x => x.replace(/\r/g, '').split(/\t/));
+  head = header ? split.shift() : assignEmptyHeader(split[0].length);
+  for (let s = 0; s < split.length; ++s) {
+    result.push(new Object());
+    for (let h = 0; h < head.length; ++h) {
+      result[s][head[h]] = split[s][h];
+    };
+  };
+  return result;
+};
+
+// Get the data and convert to JSON
+let data = convRawTsvJson(API.getData("visTsvContent").resurrect());
+
+// Save it as the new node selection
+API.createData("nodeSelection", data);
