339fe9d2d9ce563ce769c1aa9f941f4b5ed3c9ac
glm729 <georgemalone@outlook.com.au>
2021-03-05 13:07:37 +0800

    Improved loading example data in ChemInfo
    - Switched to .then chain to avoid the alert patch-up
    - Included a bit of visual output on the buttons to show progress
      loading example data

--- cheminfo/loadExampleSelection.js
+++ cheminfo/loadExampleSelection.js
@@ -1,32 +1,4 @@
-// Helper function to apply styles
-function applyStyles(domnode, css) {
-  for (let prop in css) domnode.style[prop] = css[prop];
-};
-
-// Define the button onclick function
-function buttonCb() {
-  // Fetch callback
-  function fetchCb(att) {
-    let name = "upload/testTsv.tsv";
-    let file = att.filter(a => a.filename === name)[0];
-    API.createData("exampleVisTsvContent", file);
-  };
-  // The thing to do, but with async
-  async function doThing() {
-    let c = new CouchAttachments();
-    await c.fetchList().then(fetchCb);
-    let vt = API.getData("exampleVisTsvContent");
-    if (vt === null) {
-      alert("Data not yet loaded, please wait.");
-      return;
-    };
-    fetch(vt.url)
-      .then(r => r.text())
-      .then(d => API.createData("visTsvContent", d));
-  };
-  // do.
-  doThing();
-};
+/** -- Operations -- **/
 
 // Assign button CSS attributes
 let buttonStyle = {
@@ -43,18 +15,40 @@ let buttonStyle = {
 };
 
 // Create the button and apply CSS
-let button = document.createElement("button");
-applyStyles(button, buttonStyle);
+let button = applyStyles(document.createElement("button"), buttonStyle);
 
-// Give the button some text
+// Give the button some text and assign the onclick callback
 button.innerHTML = "Use example selection";
+button.onclick = doThing;
 
-// Assign the onclick callback
-button.onclick = buttonCb;
-
-// Find and empty the sink
-let sink = document.getElementById("exampleSelection");
+// Find and empty the sink, then append the button
+let sink = document.querySelector("#exampleSelection");
 sink.innerHTML = '';
-
-// Append the button to the sink
 sink.appendChild(button);
+
+
+/** -- Function definitions -- **/
+
+// Helper function to apply styles
+function applyStyles(domnode, css) {
+  for (let prop in css) domnode.style[prop] = css[prop];
+  return domnode;
+};
+
+function fetchCb0(attachments) {
+  let name = "upload/testTsv.tsv";
+  return attachments.filter(a => a.filename === name)[0];
+};
+
+function fetchCb1(data) {
+  fetch(data.url).then(r => r.text()).then(d => {
+    API.createData("visTsvContent", d);
+    button.style.borderColor = "#0f0";
+  });
+};
+
+function doThing() {
+  button.style.borderColor = "#00f";
+  let c = new CouchAttachments();
+  c.fetchList().then(fetchCb0).then(fetchCb1);
+};
--- cheminfo/loadExampleZip.js
+++ cheminfo/loadExampleZip.js
@@ -1,13 +1,49 @@
+/** -- Operations -- **/
+
+// Define styles for the button
+let buttonStyle = {
+  "border-radius": "0px",
+  display: "block",
+  "font-family": "monospace",
+  "font-size": "14px",
+  height: "40px",
+  "margin-left": "auto",
+  "margin-right": "auto",
+  "text-align": "center",
+  padding: "4px 4px",
+  width: "144px"
+};
+
+// Create the button, assign the styles, give some text, and apply the onclick
+let button = applyStyles(document.createElement("button"), buttonStyle);
+button.innerHTML = "Run Example";
+button.onclick = doThing;
+
+// Find the sink, empty it, and append the button
+let div = document.querySelector("#runExampleButton");
+div.innerHTML = '';
+div.appendChild(button);
+
+
+/** -- Function definitions -- **/
+
 // Helper function for applying a set of CSS attributes
 function applyStyles(node, css) {
   for (let prop in css) node.style[prop] = css[prop];
+  return node;
 };
 
 // Abstracted function for processing data attached to the view
-function fetchCb(attachments) {
+function fetchCb0(attachments) {
   let name = "upload/inputZipCompressed.zip";
-  let file = attachments.filter(a => a.filename === name)[0];
-  API.createData("exampleInputZip", file);
+  return attachments.filter(a => a.filename === name)[0];
+};
+
+function fetchCb1(data) {
+  fetch(data.url).then(r => r.arrayBuffer()).then(d => {
+    API.createData("inputZip", d);
+    button.style.borderColor = "#0f0";
+  });
 };
 
 // Function to unzip a file, given an input arrayBuffer
@@ -25,40 +61,8 @@ async function unzip(content) {
 };
 
 // Do the thing!
-async function doThing() {
+function doThing() {
+  button.style.borderColor = "#00f";
   let c = new CouchAttachments();
-  await c.fetchList().then(fetchCb);
-  let iz = API.getData("exampleInputZip");
-  if (iz === null) {
-    alert("Data not yet loaded, please retry.");
-    return;
-  };
-  fetch(iz.url)
-    .then(r => r.arrayBuffer())
-    .then(d => API.createData("inputZip", d));
+  c.fetchList().then(fetchCb0).then(fetchCb1);
 };
-
-// Define styles for the button
-let buttonStyle = {
-  "border-radius": "0px",
-  display: "block",
-  "font-family": "monospace",
-  "font-size": "14px",
-  height: "40px",
-  "margin-left": "auto",
-  "margin-right": "auto",
-  "text-align": "center",
-  padding: "4px 4px",
-  width: "144px"
-};
-
-// Create the button, assign the styles, give some text, and apply the onclick
-let button = document.createElement("button");
-applyStyles(button, buttonStyle);
-button.innerHTML = "Run Example";
-button.onclick = doThing;
-
-// Find the sink, empty it, and append the button
-let div = document.querySelector("#runExampleButton");
-div.innerHTML = '';
-div.appendChild(button);
